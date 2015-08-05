f2f9959245a6ec4e7ed7826e28831ef5ea9db314
Jinho <jinhomang@gmail.com>
2015-08-05 14:36:24 +0900

    no message

---
+++ Assets/Editor/ExportMethods.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+
+public class ExportMethods {
+
+	[MenuItem ("MyMenu/ChangeRed")]
+	public static void ChangeRed() {
+		Material myMat = AssetDatabase.LoadAssetAtPath("Assets/matCube.mat", typeof(Material)) as Material;
+		myMat.color = Color.red;
+	}
+
+	[MenuItem ("MyMenu/ChangeGreen")]
+	public static void ChangeGreen() {
+		Material myMat = AssetDatabase.LoadAssetAtPath("Assets/matCube.mat", typeof(Material)) as Material;
+		myMat.color = Color.green;
+	}
+
+	
+	public static void Post(string path) {
+	
+	}
+}
